612c41e762d179d5626751050ca9ecc2d6e948d9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-08-07 12:38:56 -0300

    feat: req 01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    headers = {'user-agent': 'Fake user-agent'}
+
+    try:
+        response = requests.get(url, headers=headers, timeout=3)
+        time.sleep(1)
+        return response.text if response.status_code == 200 else None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
